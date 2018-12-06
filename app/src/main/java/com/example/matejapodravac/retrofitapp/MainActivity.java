e45e310ed846f9a78756981c2454c417f95f5478
Mateja Podravac <33102299+MPodravac@users.noreply.github.com>
2018-12-06 17:23:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/matejapodravac/retrofitapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.matejapodravac.retrofitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.matejapodravac.retrofitapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
